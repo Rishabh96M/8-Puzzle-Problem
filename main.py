b7eb14a80a030e0127acfbd8a11fabefa9d5ccbd
Rishabh Mukund <rishabh.m96@gmail.com>
2022-02-18 17:46:02 -0500

    Implemented backtracking

--- main.py
+++ main.py
@@ -31,54 +31,62 @@ def BFS(init, end):
         row, col = position(0, curr_node)
 
         if (row + 1) < 3:
-            # Move Down
+            # Move Up
             new_node = swap_ele(curr_node, (row, col), (row + 1, col))
             if new_node not in visited:
-                if (new_node == end):
-                    break
                 visited.append(new_node)
                 parent_index.append(visited.index(curr_node))
+                if (new_node == end):
+                    break
                 queue.insert(0, new_node)
 
         if (row - 1) > -1:
-            # Move Up
+            # Move Down
             new_node = swap_ele(curr_node, (row, col), (row - 1, col))
             if new_node not in visited:
-                if (new_node == end):
-                    break
                 visited.append(new_node)
                 parent_index.append(visited.index(curr_node))
+                if (new_node == end):
+                    break
                 queue.insert(0, new_node)
 
         if (col + 1) < 3:
-            # Move Right
+            # Move Left
             new_node = swap_ele(curr_node, (row, col), (row, col + 1))
             if new_node not in visited:
-                if (new_node == end):
-                    break
                 visited.append(new_node)
                 parent_index.append(visited.index(curr_node))
+                if (new_node == end):
+                    break
                 queue.insert(0, new_node)
 
         if (col - 1) > -1:
-            # Move Left
+            # Move Right
             new_node = swap_ele(curr_node, (row, col), (row, col - 1))
             if new_node not in visited:
-                if (new_node == end):
-                    break
                 visited.append(new_node)
                 parent_index.append(visited.index(curr_node))
+                if (new_node == end):
+                    break
                 queue.insert(0, new_node)
+
     print('reached')
-    print(end)
-    print(curr_node)
+    return visited, parent_index
+
+
+def backtracking(visited, parent_index):
+    curr_node = visited[-1]
+    path = []
     p_idx = 1
+    node_idx = []
     while p_idx:
-        idx = visited.index(curr_node)
-        p_idx = parent_index[idx]
-        print(visited[p_idx])
+        path.append(curr_node)
+        node_idx.append(visited.index(curr_node))
+        p_idx = parent_index[visited.index(curr_node)]
         curr_node = visited[p_idx]
-    return 0, 0
+    path.append(curr_node)
+    print(path[::-1])
+    print(node_idx[::-1])
 
 
 if __name__ == '__main__':
@@ -87,4 +95,5 @@ if __name__ == '__main__':
 
     goal = [[1, 4, 7], [2, 5, 8], [3, 6, 0]]
 
-    path, actions = BFS(start, goal)
+    visited, parent_index = BFS(start, goal)
+    backtracking(visited, parent_index)
